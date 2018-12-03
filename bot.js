ff9ae0ab8393419a01ffe44599ee69d2a28817fe
ekomekoteko <45575162+ekomekoteko@users.noreply.github.com>
2018-12-03 23:34:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -269,6 +269,46 @@ client.on('message', msg => {
 
 
 
+
+
+
+////////////////////////////////////// كود برودكاست للرتبه معينه
+
+
+
+          client.on('message' , message => {
+      if(message.author.bot) return;
+
+      if(message.content.startsWith(".bk")) {
+        if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+        let args = message.content.split(" ").slice(2);
+     var codes = args.join(' ')
+
+        if(!codes) {
+          message.channel.send("قم بكتابة الرسالة | !rolebc @everyone message")
+            return;
+        }
+
+
+              var role = message.mentions.roles.first();
+                if(!role) {
+                  message.reply("لا توجد رتبة بهذا الاسم")
+                    return;
+                }
+            message.guild.members.filter(m => m.roles.get(role.id)).forEach(n => {
+              n.send(
+              "**" + "السيرفر :" + "\n" +
+              `${message.guild.name}` + "\n" +
+              "المرسل :" + "\n" +
+              `${message.author.tag}` + "\n" +
+              "الرسالة :" + "\n" +
+              `${codes}` + "**"
+              )
+            })
+            message.channel.send(`لقد تم ارسال هذه الرسالة الى ${message.guild.members.filter(m => m.roles.get(role.id)).size} عضو`)
+        }
+    });
+
  
  
  
