ee6acbdc7dfc2bc4fc171687d36ee0f968833833
ekomekoteko <45575162+ekomekoteko@users.noreply.github.com>
2018-12-03 22:47:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,23 @@ const pretty = require('pretty-ms')
  
  
  
- 
+ client.on('ready', function(){
+    var ms = 10000 ;
+    var setGame = [' ?help ','?invite | ?support ' ];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`https://www.twitch.tv/FirstBot]`);
+    }, ms);
+
+})
  
  
  
