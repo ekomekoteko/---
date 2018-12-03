2f72dbd22d99d264f34357e0048ea7586ab5f54f
ekomekoteko <45575162+ekomekoteko@users.noreply.github.com>
2018-12-03 23:39:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -361,8 +361,28 @@ room.send(tests);
 }
 });
 
+////////////////////////////// يعطيك صوره السيرفر
+
+ client.on("message", message => {
+
+          if(!message.channel.guild) return;
+   if(message.author.bot) return;
+      if(message.content === prefix + "simage"){
+          const embed = new Discord.RichEmbed()
+
+      .setTitle(`This is  ** ${message.guild.name} **  Photo !`)
+  .setAuthor(message.author.username, message.guild.iconrURL)
+    .setColor(0x164fe3)
+    .setImage(message.guild.iconURL)
+    .setURL(message.guild.iconrURL)
+                    .setTimestamp()
+
+   message.channel.send({embed});
+      }
+  });
 
 
- 
+
+
  
  client.login(process.env.BOT_TOKEN);
