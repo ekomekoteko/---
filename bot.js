006e92ffd7dccac71ef59f19105fc1640aa26417
ekomekoteko <45575162+ekomekoteko@users.noreply.github.com>
2018-12-03 23:37:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -310,6 +310,59 @@ client.on('message', msg => {
     });
 
  
+
+
+
+
+////////////////////////////////////// كود يكرر الكلام بالشات كل شوى
+
+
+
+client.on('message' , async message => {
+            if(message.content.startsWith(prefix + "ads")) {
+     await message.channel.send("`ارسال الرساله .`").then(e => {
+    let filter = m => m.author.id === message.author.id
+    let tests = '';
+    let time = '';
+    let channel = '';
+    let chaTests = message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
+    .then(collected => {
+      tests = collected.first().content
+      collected.first().delete()
+e.edit("`تكرار الرساله كل ....... دقائق`")
+let chaTime = message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
+.then(co => {
+if(isNaN(co.first().content)) return message.reply("`الوقت بالدقائق ! ارقام فقطٍ`");
+if(co.first().content > 1500 || co.first().content < 1) return message.channel.send("`لا اقل من دقيقه ولا اكثر من يوم`")
+  time = co.first().content
+co.first().delete()
+  e.edit("`ادخل اسم الروم`")
+  let chaChannel = message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
+.then(col => {
+  channel = col.first().content
+col.first().delete()
+  e.edit("`جاري اعداد المعلومات الرجاء الانتظاار...`").then(b => {
+              setTimeout(() => {
+    b.edit(`** تم اعداد المعلومات بنجاح .**`)
+        },2000);
+  })
+  var room = message.guild.channels.find('name' , channel)
+  if(!room) return;
+  if (room) {
+setInterval(() => {
+room.send(tests);
+}, time*60000)
+  }
+})
+})
+})
+
+})
+}
+});
+
+
+
  
  
  client.login(process.env.BOT_TOKEN);
