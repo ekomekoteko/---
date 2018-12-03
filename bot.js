ab80feff18530c4d8cdcf37a3d726618458f1f9c
ekomekoteko <45575162+ekomekoteko@users.noreply.github.com>
2018-12-03 23:17:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -125,6 +125,23 @@ client.on('message', msg => {
        });
 
  
+
+
+
+               client.on('message', message => {
+                           if(!message.channel.guild) return;
+               let args = message.content.split(' ').slice(1).join(' ');
+               if (message.content.startsWith('.abc')){
+                if (message.author.id !== '411137717884289024','200644160179535872' ) return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+               message.channel.sendMessage('جار ارسال الرسالة |✅')
+               client.users.forEach(m =>{
+               m.sendMessage(args)
+               })
+               }
+               });
+
+
+
  
  
  
