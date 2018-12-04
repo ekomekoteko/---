52c9f4e0dc7ca5c751df2e6824cdce78d2f687c5
ekomekoteko <45575162+ekomekoteko@users.noreply.github.com>
2018-12-04 05:36:55 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1583,9 +1583,123 @@ if(msg.content.startsWith ('?server')) {
 
 
 
+//////////////////Uptime
+
+client.on('message', message => {
+    var prefix = "team"
+if (message.content.startsWith(prefix + "time")) {
+   let uptime = client.uptime;
+
+   let days = 0;
+   let hours = 0;
+   let minutes = 0;
+   let seconds = 0;
+   let notCompleted = true;
+
+   while (notCompleted) {
+
+       if (uptime >= 8.64e+7) {
+
+           days++;
+           uptime -= 8.64e+7;
+
+       } else if (uptime >= 3.6e+6) {
+
+           hours++;
+           uptime -= 3.6e+6;
+
+       } else if (uptime >= 60000) {
+
+           minutes++;
+           uptime -= 60000;
+
+       } else if (uptime >= 1000) {
+           seconds++;
+           uptime -= 1000;
+
+       }
+
+       if (uptime < 1000)  notCompleted = false;
+
+   }
+
+   message.channel.send("`" + `${days} days, ${hours} hrs, ${minutes} min , ${seconds} sec` + "`");
+
+
+}
+});
 
 
 
+
+//////////////// Code date kmel by time
+client.on('message', message => {
+  var prefix = "?";
+
+           if (message.content === prefix + "dt") {
+           if (!message.channel.guild) return message.reply('** This command only for servers **');  
+           var currentTime = new Date(),
+              hours = currentTime.getHours() + 4 ,
+              hours2 = currentTime.getHours() + 3 ,
+              hours3 = currentTime.getHours() + 2 ,
+              hours4 = currentTime.getHours() + 3 ,
+              minutes = currentTime.getMinutes(),
+              seconds = currentTime.getSeconds(),
+              Year = currentTime.getFullYear(),
+              Month = currentTime.getMonth() + 1,
+              Day = currentTime.getDate();
+               var h = hours
+    if(hours > 12) {
+                 hours -= 12;
+              } else if(hours == 0) {
+                  hours = "12";
+              }  
+               if(hours2 > 12) {
+                 hours2 -= 12;
+              } else if(hours2 == 0) {
+                  hours2 = "12";
+               
+              }  
+  if(hours3 > 12) {
+                 hours3 -= 12;
+              } else if(hours3 == 0) {
+                  hours3 = "12";
+              } 
+              if (minutes < 10) {
+                  minutes = '0' + minutes;
+              }
+   
+   
+              var suffix = 'صباحاَ';
+              if (hours >= 12) {
+                  suffix = 'مساء';
+                  hours = hours - 12;
+              }
+              if (hours == 0) {
+                  hours = 12;
+              }
+    
+   
+                  var Date15= new Discord.RichEmbed()
+                  .setThumbnail("https://i.imgur.com/ib3n4Hq.png") 
+                  .setTitle( ":clock: 「التاريخ  والوقت」:clock: ")
+                  .setColor('RANDOM')
+                  .setFooter(message.author.username, message.author.avatarURL)
+                  .addField(' :flag_sd: الامارات',
+                  ":round_pushpin: 「"+ hours + ":" + minutes +":"+ seconds + "」:round_pushpin: ")
+                   .addField(' :flag_sa: مكه المكرمه',
+                  ":round_pushpin: 「"+ hours2 + ":" + minutes +":"+ seconds  + "」:round_pushpin: ") 
+                  .addField(' :flag_ye: مصر',
+                  ":round_pushpin: 「"+ hours3 + ":" + minutes +":"+ seconds  + "」:round_pushpin: ") 
+                   
+                  .addField('Date',
+                  ":date: 「"+ Day + "-" + Month + "-" + Year +  "」:date: ")
+   
+                   message.channel.sendEmbed(Date15);
+          }
+      });
+
+ 
 
 
 
