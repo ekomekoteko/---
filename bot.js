003686b477a79f62088202e3d5763a38b2ce349a
ekomekoteko <45575162+ekomekoteko@users.noreply.github.com>
2018-12-05 17:46:51 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,28 +47,7 @@ client.on('ready', () => {
 
 
 
-client.on("message", message => {
-	var prefix = "???";
- if (message.content === "?help") {
-  const embed = new Discord.RichEmbed()  
-      .setColor("#000000") 
-      .setDescription(`
-	
-     **Help Fisrt Bot**🔥
-		 
-      🔔 Orders Public: Type:?help-public » To Get Orders Public
-  🔐 Orders Admin: Type: ?help-Admin » To Get Orders Admin
-     🎶 Orders Music: Type: ?help-music » To Get Orders Music
-  🎲 Orders Games: Type: ?help-games » To Get Orders GAMES
-     🎊 Orders Welcome: Type: ?help-welcomer »  To Get Orders Welcomer
- 📬 Orders BoradCast: Type: ?help-bc » To Get Orders BoradCast
-
-          `)
-   message.channel.sendEmbed(embed)
-    
-   }
-   }); 
-
+x
 
 
 
