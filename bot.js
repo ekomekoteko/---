9f11a289f94dc38354ae7430cf8879b1c1206371
ekomekoteko <45575162+ekomekoteko@users.noreply.github.com>
2018-12-05 17:45:55 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,36 @@
+
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const tpoints = {};
+const vpoints = {};
+const jimp = require("jimp");
 const prefix = "?"
- 
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+ client.user.setActivity("",{type: 'watching'})
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+}); 
  
  client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
